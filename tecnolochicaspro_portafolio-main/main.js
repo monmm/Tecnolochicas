129a9b69a51646f0bf983bd13caf75615047ce4c
Mónica Miranda <monmm@ciencias.unam.mx>
2023-04-29 01:21:43 -0600

    Portafolio web Monica Miranda

--- tecnolochicaspro_portafolio-main/main.js
+++ tecnolochicaspro_portafolio-main/main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Desarrollo sitios web y escribo artículos sobre programación.')
+  .typeString('Mi superpoder es el codigo, cual es el tuyo?')
   .pauseFor(200)
   .deleteChars(10)
   .start();
